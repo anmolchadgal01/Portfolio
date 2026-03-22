a17140e6474d231afbd8fe269ef75969075bae86
Rohit Kumar <your-email@gmail.com>
2026-03-22 18:16:49 +0530

    updated text

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -49,7 +49,7 @@ export const Hero=()=>{
         <div className="space-y-8">
             <div className="animate-fade-in">
                 <span className="inline-flex items-center gap-2 px-4 py-2 rounded-full text-sm text-primary">
-                 <span className="w-2 h-2 bg-primary rounded-full animate-pulse"/>Full-Stack Developer - React Specialistn
+                 <span className="w-2 h-2 bg-primary rounded-full animate-pulse"/>Full-Stack Developer - React Specialist
                 </span>
                
             </div>
